6e88d73e33488d029a38731407db59ce8b531d74
hqvjet <viethq.1906@gmail.com>
2025-08-24 23:01:57 +0700

    fix ico not shown error

---
+++ frontend/src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/logo.ico" />
+      <link rel="shortcut icon" href="/logo.ico" />
+      <link rel="apple-touch-icon" href="/logo.ico" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+    </>
+  );
+}
